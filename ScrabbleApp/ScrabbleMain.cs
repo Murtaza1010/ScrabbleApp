a17665e918bf02e473590d4c7813f49f34808b6a
MK <mortezakarim0@gmail.com>
2023-04-15 22:47:00 +0100

    Add project files.

---
+++ ScrabbleApp/ScrabbleMain.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+class ScrabbleMain
+{
+    static void Main(string[] args)
+    {
+        ScrabbleScore scorer = new ScrabbleScore();
+        Console.WriteLine("Enter the word to Calculate your Score ( - to end):");
+        string word = Console.ReadLine();
+        while (word != "-") 
+        {
+            Console.WriteLine("The Score for " + word + " is " + scorer.GetScrabbleScore(word));
+            Console.WriteLine("Enter the word to Calculate your Score ( - to end):");
+            word = Console.ReadLine();
+        }
+        
+    }
+}
---
+++ ScrabbleApp/ScrabbleScore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ScrabbleScore
+{
+    Dictionary<int, string> PointScore = new Dictionary<int, string>
+    {
+        {1, "AEIOULNSTR" },
+        {2, "DG" },
+        {3, "BCMP" },
+        {4, "FHVWY" },
+        {5, "K" },
+        {8, "JX" },
+        {10, "QZ" },
+    };
+
+    public int GetScrabbleScore(string word)
+    {
+        int wordScore = 0;
+        foreach (char letter in word.ToUpper())
+        {
+            foreach(int score in PointScore.Keys)
+            {
+                if (PointScore[score].Contains(word))
+                {
+                    wordScore += score;
+                }
+            }
+
+        }
+        return wordScore;
+    }
+
+}
